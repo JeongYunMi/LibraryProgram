56a9bf08e2d13f42bee4ea81b8f2594aedf1bcb6
JeongYunMi <tamd5971@gmail.com>
2021-11-12 10:46:52 +0900

    프로젝트 생성

---
+++ LibraryProgram/src/kr/ac/hansei/java/Program.java
@@ -0,0 +1,13 @@
+/*
+ * class¸í: Program
+ * last update: 21.11.12
+ * Contributor : kimjaehoon, jeongyunmi
+ * summary : It's a class that will contain a large GUI frame.
+ * */
+
+package kr.ac.hansei.java;
+
+
+public class Program {
+
+}
