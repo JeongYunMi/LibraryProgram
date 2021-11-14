113079b48fe05190e14e334ad075e5c6f04ee1ee
JeongYunMi <tamd5971@gmail.com>
2021-11-14 12:40:08 +0900

    Add memberManagement menu label

--- LibraryProgram/src/kr/ac/hansei/java/Program.java
+++ LibraryProgram/src/kr/ac/hansei/java/Program.java
@@ -7,7 +7,11 @@
 
 package kr.ac.hansei.java;
 
+import java.awt.BorderLayout;
+
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 /* 회원정보를 담는 클래스입니다.*/
 class Member{
@@ -66,8 +70,17 @@ class Member{
 
 public class Program {
 	public static void main(String[] args) {
-		
 		JFrame GuiFrame = new JFrame(); //JFrame 객체 생성(도서관리 프로그램의 가장 큰 틀)
+		
+		JPanel TopMenuName = new JPanel();
+		
+		JLabel menuName = new JLabel("회원 관리");	
+		TopMenuName.add(menuName, BorderLayout.NORTH);
+		
+		GuiFrame.setLayout(new BorderLayout());
+		GuiFrame.add(TopMenuName);
+		
+		
 		GuiFrame.setTitle("LibraryProgram"); //Title 이름 정하기
 		GuiFrame.setSize(1600, 1000); // 프레임 크기
 		GuiFrame.setVisible(true); //프레임 출력
