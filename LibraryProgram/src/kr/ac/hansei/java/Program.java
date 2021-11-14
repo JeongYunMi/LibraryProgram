e8948868b488186d840cbfe4a1e720a4d077b29b
rlawogns1121@gmail.com <rlawogns1121@gmail.com>
2021-11-14 23:19:51 +0900

    Add bookManagement menu label

--- LibraryProgram/src/kr/ac/hansei/java/Program.java
+++ LibraryProgram/src/kr/ac/hansei/java/Program.java
@@ -7,8 +7,12 @@
 
 package kr.ac.hansei.java;
 
+import java.awt.BorderLayout;
 import java.util.Vector;
+
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 /*
  * Book클래스에는 책시리얼넘버, 책이름, 저자,출판일, 위치, 출판사, 대여일, 반납예정일, 대여유무가 담겨있습니다
@@ -91,6 +95,15 @@ public class Program {
 	public static void main(String[] args) {
 		
 		JFrame GuiFrame = new JFrame(); //JFrame 객체 생성(도서관리 프로그램의 가장 큰 틀)
+		
+		JPanel TopMenuName = new JPanel();
+		
+		JLabel MenuName = new JLabel("도서 관리");
+		TopMenuName.add(MenuName, BorderLayout.NORTH);
+		
+		GuiFrame.setLayout(new BorderLayout());
+		GuiFrame.add(TopMenuName);
+		
 		GuiFrame.setTitle("LibraryProgram"); //Title 이름 정하기
 		GuiFrame.setSize(1600, 1000); // 프레임 크기
 		GuiFrame.setVisible(true); //프레임 출력
