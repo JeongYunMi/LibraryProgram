d91eb0720264ccf9528f18752f04dd8ed439491b
JeongYunMi <tamd5971@gmail.com>
2021-11-16 02:42:04 +0900

    Add MemberManagement.java - 회원 검색, 조회, 추가, 삭제 버튼 GUI

--- LibraryProgram/src/kr/ac/hansei/java/MemberManagementGUI.java
+++ LibraryProgram/src/kr/ac/hansei/java/MemberManagementGUI.java
@@ -9,6 +9,11 @@ import java.awt.Font;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.JButton;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 
 /*회원정보를 담는 클래스입니다.*/
 class Member{
@@ -68,6 +73,7 @@ public class MemberManagementGUI {
 
 	private JFrame frame;
 	private JTable table;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -97,7 +103,7 @@ public class MemberManagementGUI {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 893, 540);
+		frame.setBounds(100, 100, 880, 540);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(new BorderLayout(0, 0));
 		
@@ -113,13 +119,34 @@ public class MemberManagementGUI {
 				{"2019XXXXX", "김재훈", "5", "대출 가능"},
 				{"201910063", "정윤미", "3", "연체"}
 		};
+		
+		JPanel panel_1 = new JPanel();             //버튼(회원검색, 조회, 추가, 삭제 등)을 담을 panel
+		panel.add(panel_1);
+		//panel_1.setLayout(new GridLayout(0, 2, 0, 0));
+		
+		JLabel lblNewLabel_1 = new JLabel("회원 검색");
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
+		panel_1.add(lblNewLabel_1);
+		
+		textField = new JTextField();
+		panel_1.add(textField);
+		textField.setColumns(10);
+		
+		JButton btnNewButton_3 = new JButton("검색");
+		panel_1.add(btnNewButton_3);
+		
+		JButton btnNewButton_1 = new JButton("회원 추가");
+		panel_1.add(btnNewButton_1);
+		
+		JButton btnNewButton = new JButton("선택한 회원정보 조회");
+		panel_1.add(btnNewButton);
+		
+		JButton btnNewButton_2 = new JButton("회원 삭제");
+		panel_1.add(btnNewButton_2);
 		//panel.setLayout(new BorderLayout());
 		JTable MemberTable= new JTable(MemberInfo, header); // 회원 정보를 담을 JTable 
 		JScrollPane MemberTableScroll = new JScrollPane(MemberTable);
 		panel.add(MemberTableScroll, BorderLayout.CENTER);
-		
-		JPanel panel_1 = new JPanel();
-		panel.add(panel_1);
 		
 		
 	}
